1d2f31fd863570a8ae36fdce86d28095a4038ba8
K <byeolharang@kakao.com>
2021-05-01 07:09:27 +0000

    .

---
+++ src/005.ConditionStatement/IfElseIfStatement.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int num;
+	
+	printf("아무 숫자나 입력해보세요 : ");
+	scanf("%d", &num);
+	
+	if (num == 7)
+	{
+		printf("행운의 숫자 7이군요!\n");
+	}
+	else if (num == 4)
+	{
+		printf("죽음의 숫자 4인가요 ;;; \n");
+	}
+	else
+	{
+		printf("그냥 평범한 숫자 %c \n", num);
+	}
+	
+	return 0;
+}
