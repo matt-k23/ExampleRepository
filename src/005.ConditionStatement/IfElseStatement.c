f214229702af0e2c9e0f72c5170adb43ef0d5d3b
K <byeolharang@kakao.com>
2021-05-02 06:18:50 +0000

    .

--- src/005.ConditionStatement/IfElseStatement.c
+++ src/005.ConditionStatement/IfElseStatement.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-int main()
+int main() 
 {
-	int num;
-	
-	printf("아무 숫자나 입력해보세요 : ");
-	scanf("%d", &num);
-	
-	if (num == 7)
-	{
-		printf("행운의 숫자 7이군요!\n");
-	}
-	else
-	{
-		printf("그냥 보통 숫자인 %d(을)를 입력했군요\n", num);
-	}
-	
-	return 0;
+    int num;
+
+    printf("아무 숫자나 입력해보세요 : ");
+    scanf("%d", &num);
+
+    if (num == 7) 
+    {
+        printf("행운의 숫자 7이군요!\n");
+    }
+    else 
+    {
+        printf("그냥 보통 숫자인 %d(을)를 입력했군요\n", num);
+    }
+    
+    return 0;
 }
