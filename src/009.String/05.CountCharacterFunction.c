df9ad615c6aa2aa88aa6aa49ca52eb86d9f1f305
K <byeolharang@kakao.com>
2021-05-09 08:58:06 +0000

    .

--- src/009.String/05.CountCharacterFunction.c
+++ src/009.String/05.CountCharacterFunction.c
@@ -11,6 +11,8 @@ int str_length(char *str) {
 int main() {
 	char str[] = {"what is your name?"};
 	
+	int count = str_length(str);
+	
 	printf("%s \n", str);
 	printf("위 문자열의 길이 : %d \n", str_length(str));
 	
---
+++ src/999.Test/CoderJungol.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int a, b;
+	int *pa, *pb;
+	
+	scanf("%d %d", &a, &b);
+	pa = &a;
+	pb = &b;
+	
+	printf("%d + %d = %d \n", *pa, *pb, *pa + *pb);
+	printf("%d - %d = %d \n", *pa, *pb, *pa - *pb);
+	printf("%d * %d = %d \n", *pa, *pb, *pa * *pb);
+	printf("%d / %d = %d \n", *pa, *pb, *pa / *pb);
+	
+	return 0;
+}
